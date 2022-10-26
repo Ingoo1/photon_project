fea33d4c0fca97acf6e63221b305811c65b47130
Maksym_enKO <ingool.ua@gmail.com>
2022-10-26 04:05:36 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,4 @@
+const auth = ''; //ADD THE AUTH KEY
+const gallery = document.querySelector('.gallery');
+const searchInput = document.querySelector('.search-input');
+const form = document.querySelector('.search-form');
