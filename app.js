6415b6799a8ca0be31a21376f86f0545d72216d5
Maksym_enKO <ingool.ua@gmail.com>
2022-10-28 21:39:51 +0300

    fetching the images

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
-const auth = ''; //ADD THE AUTH KEY
+const auth = '563492ad6f91700001000001281efcdae8de4c3dbd0f360e48e75719';
 const gallery = document.querySelector('.gallery');
 const searchInput = document.querySelector('.search-input');
 const form = document.querySelector('.search-form');
+let searchValue;
+
+async function curatedPhotos() {
+  const dataFetch = await fetch(
+    'https://api.pexels.com/v1/curated?per_page=15&page=1',
+    {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        Authorization: auth,
+      },
+    }
+  );
+  const data = await dataFetch.json();
+  data.photos.forEach((photo) => {
+    const galleryImg = document.createElement('div');
+    galleryImg.classList.add('gallery-img');
+    galleryImg.innerHTML = `<img src=${photo.src.large}>
+        <p>${photo.photographer}</p>
+        `;
+    gallery.appendChild(galleryImg);
+  });
+}
+
+curatedPhotos();
