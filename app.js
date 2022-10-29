ee16e283c6cd94e10b10927ce3ca2b927edbfc5a
Maksym_enKO <ingool.ua@gmail.com>
2022-10-30 01:07:58 +0300

    search clear and download button

--- app.js
+++ app.js
@@ -32,8 +32,12 @@ function generetePictures(data) {
   data.photos.forEach((photo) => {
     const galleryImg = document.createElement('div');
     galleryImg.classList.add('gallery-img');
-    galleryImg.innerHTML = `<img src=${photo.src.large}>
-        <p>${photo.photographer}</p>
+    galleryImg.innerHTML = `
+        <div class="gallery-info">
+          <p>${photo.photographer}</p>
+          <a href=${photo.src.original}>Download</a>
+        </div>
+        <img src=${photo.src.large}>
         `;
     gallery.appendChild(galleryImg);
   });
@@ -52,7 +56,13 @@ async function searchPhotos(query) {
     `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`
   );
 
+  clear();
   generetePictures(data);
+}
+
+function clear() {
+  gallery.innerHTML = '';
+  searchInput.value = '';
 }
 
 curatedPhotos();
